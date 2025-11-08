31a68c53f3420201b2295d1cc33a77117185dcc8
thelechen <thelechen@gmail.com>
2025-11-08 08:32:42 +0000

    Deploying to gh-pages from @ thelechen/thelechen.github.io@79c7a16f8320133e13d297d9c20cba764c88bff1 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "LChenGit/lchengit.github.io",
+    "data-repo": "thelechen/thelechen.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
